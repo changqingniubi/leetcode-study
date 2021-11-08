4cb1d818e03cbf1eade743f48f75fa5453d09c9e
changqing <qing.chang@eeoa.com>
2021-11-08 21:03:38 +0800

    链表

---
+++ src/206. 反转链表.js
@@ -0,0 +1,34 @@
+/*
+ * @Description: 
+ * @Author: changqing
+ * @Date: 2021-11-08 19:58:32
+ * @LastEditTime: 2021-11-08 21:00:26
+ * @LastEditors: changqing
+ * @Usage: 
+ */
+//1. 迭代解法
+var reverseList = function(head) {
+  if(!head){
+    return head;
+  }
+  let pre=null;
+  let curr=head;
+  while(curr){
+    let next = curr.next;
+    curr.next=pre;
+    pre=curr;
+    curr=next;
+  }
+  return pre;
+};
+
+
+// 2. 递归解法
+
+var reverseList = function(head) {
+  if (head == null || head.next == null) return head
+  const p = reverseList(head.next)
+  head.next.next = head
+  head.next = null
+  return p
+};
---
+++ src/92. 反转链表 II.js
@@ -0,0 +1,14 @@
+/*
+ * @Description: 
+ * @Author: changqing
+ * @Date: 2021-11-08 19:46:04
+ * @LastEditTime: 2021-11-08 19:50:18
+ * @LastEditors: changqing
+ * @Usage: 
+ */
+
+
+
+var reverseBetween = function(head, left, right) {
+   
+};
